ecb44d0d934762f03041021349ad94ab06543bc8
Nolan MacDonald <66697853+nolmacdonald@users.noreply.github.com>
2024-10-05 14:34:22 -0400

    Helper functions replace_team_names and replace_player_names now include documentation formatted for use with roxygen2

--- R/helpers_rosters.R
+++ R/helpers_rosters.R
@@ -6,7 +6,27 @@
 #   styler::tidyverse_style(), lintr::use_lintr(type = "tidyverse")
 ################################################################################
 
-# Function to replace player names
+#' Helper - Cleanup Player Names
+#'
+#' @description
+#' String parsing helper to cleanup player names for continuity.
+#'
+#' @details
+#'  The function `replace_player_names` reads a dataframe that includes player
+#'  names and uses `str_replace` to clean up names. This can be used across the
+#'  package when joining data such that all player names are the same.
+#'  The function strips names of suffixes like Jr., Sr., II, III. Also, the
+#'  first names that are abbreviations like D.J. Moore and D.K. Metcalf have
+#'  the periods stripped from the initials. All other player names are
+#'  specified to say the same. Some specific cases are defined explicitly.
+#'
+#' @param df Dataframe with player column to clean up names
+#'
+#' @return Dataframe with cleaned up player names
+#'
+#' @author Nolan MacDonald
+#'
+#' @export
 replace_player_names <- function(df) {
   df %>%
     dplyr::mutate(
@@ -32,7 +52,27 @@ replace_player_names <- function(df) {
     )
 }
 
-# Function to replace team names
+#' Helper - Cleanup Team Abbreviations
+#'
+#' @description
+#' String parsing helper to cleanup team abbreviations for continuity.
+#'
+#' @details
+#'  The function `team_player_names` reads a dataframe that includes team
+#'  names as `team` and uses `str_replace` to replace the team abbreviation.
+#'  This can be used across the package when joining data such that all team
+#'  names are the same. Team specific cases are defined explicitly, to
+#'  alleviate the issue of some data differences. For example, `nflreadr` roster
+#'  data lists the Los Angeles Rams as LA, while `nflfastR` play-by-play data
+#'  lists the abbreviation as LAR.
+#'
+#' @param df Dataframe with team column to clean up abbreviations
+#'
+#' @return Dataframe with cleaned up team abbreviations
+#'
+#' @author Nolan MacDonald
+#'
+#' @export
 replace_team_names <- function(df) {
   df %>%
     dplyr::mutate(team = dplyr::case_when(
