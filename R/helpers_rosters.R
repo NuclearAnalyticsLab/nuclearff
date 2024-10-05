2d2d228191d38f79d85ef2009d2da007f5d3290a
Nolan MacDonald <66697853+nolmacdonald@users.noreply.github.com>
2024-10-05 14:09:37 -0400

    Helpers to clean up rosters like player names and teams for continuity

---
+++ R/helpers_rosters.R
@@ -0,0 +1,42 @@
+################################################################################
+# Author: Nolan MacDonald
+# Purpose:
+#   Helper functions to clean player names and teams for continuity
+# Code Style Guide:
+#   styler::tidyverse_style(), lintr::use_lintr(type = "tidyverse")
+################################################################################
+
+# Function to replace player names
+replace_player_names <- function(df) {
+  df %>%
+    dplyr::mutate(
+      player = dplyr::case_when(
+        position == "RB" & player == "De'Von Achane" ~ "Devon Achane",
+        position == "RB" & player == "Kenneth Walker III" ~ "Kenneth Walker",
+        position == "RB" & player == "Jeff Wilson" ~ "Jeffery Wilson",
+        position == "RB" & player == "Chris Brooks" ~ "Christopher Brooks",
+        position == "WR" & player == "Gabriel Davis" ~ "Gabe Davis",
+        position == "WR" & player == "D.K. Metcalf" ~ "DK Metcalf",
+        position == "TE" & player == "AJ Barner" ~ "A.J. Barner",
+        # Remove Jr. or Sr. when detected
+        stringr::str_detect(player, "\\s*(Jr\\.|Sr\\.)$") ~ stringr::str_replace(player, "\\s*(Jr\\.|Sr\\.)$", ""),
+        # Remove II or III when detected
+        stringr::str_detect(player, "\\s*(II|III)$") ~ stringr::str_replace(player, "\\s*(II|III)$", ""),
+        # Strip initials
+        TRUE ~ stringr::str_replace_all(player, "\\b([A-Z])\\.", "\\1") %>%
+          stringr::str_replace_all("\\s+", " ") %>%
+          stringr::str_trim(),
+        # Leave all other names unchanged
+        TRUE ~ player
+      )
+    )
+}
+
+# Function to replace team names
+replace_team_names <- function(df) {
+  df %>%
+    dplyr::mutate(team = dplyr::case_when(
+      team == "LA" ~ "LAR",
+      TRUE ~ team  # Leave all other names unchanged
+    ))
+}
